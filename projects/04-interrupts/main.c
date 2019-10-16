d97bd9b5e8b4d0f4e68a16e79ff7a6131d8e24f7
lab661 <lab661@vutbr.cz>
2019-10-16 09:43:32 +0200

    day of interrupts

---
+++ projects/04-interrupts/main.c
@@ -0,0 +1,77 @@
+    /*
+ * ---------------------------------------------------------------------
+ * Author:      Jan Hocz, Martin Kosut
+ *              Dept. of Radio Electronics, Brno Univ. of Technology
+ * Created:     2018-10-09
+ * Last update: 2019-10-09
+ * Platform:    ATmega328P, AVR 8-bit Toolchain 3.6.2
+ * ---------------------------------------------------------------------
+ */
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "gpio.h"
+#include "timer.h"
+
+/* Typedef -----------------------------------------------------------*/
+/* Define ------------------------------------------------------------*/
+#define LED_PIN0     PB5
+#define LED_PIN1     PB0
+#define BUTTON       PD2
+
+/* Variables ---------------------------------------------------------*/
+/* Function prototypes -----------------------------------------------*/
+
+/* Functions ---------------------------------------------------------*/
+/**
+  * Brief:  Main program. Blink a LED with delay function.
+  * Input:  None
+  * Return: None
+  */
+  
+    int i = 0;
+
+int main(void)
+{
+    
+    
+    GPIO_config_output(&DDRB, LED_PIN0);
+    GPIO_config_output(&DDRB, LED_PIN1);
+
+    
+    TIM_config_prescaler(TIM0, TIM_PRESC_1024);
+    TIM_config_interrupt(TIM0, TIM_OVERFLOW_ENABLE);
+
+    TIM_config_prescaler(TIM1, TIM_PRESC_256);
+    TIM_config_interrupt(TIM1, TIM_OVERFLOW_ENABLE);
+
+    sei();
+    
+    /* Infinite loop */
+
+    for (;;)
+    {
+       
+    }
+	
+    return (0);
+}
+
+ISR(TIMER0_OVF_vect)
+{
+    i++;
+    if(i==10)
+    {
+        i=0;
+        GPIO_toggle(&PORTB, LED_PIN1);
+    }
+}
+
+ISR(TIMER1_OVF_vect)
+{
+    
+    GPIO_toggle(&PORTB, LED_PIN0);
+    
+}
