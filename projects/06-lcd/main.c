66b05cee4e128e81b66875bb8162d8e7581ade40
lab661 <lab661@vutbr.cz>
2019-10-30 08:50:23 +0100

    testing basic lce functions

---
+++ projects/06-lcd/main.c
@@ -0,0 +1,93 @@
+/*
+ * ---------------------------------------------------------------------
+ * Author:      Tomas Fryza
+ *              Dept. of Radio Electronics, Brno Univ. of Technology
+ * Created:     2018-10-16
+ * Last update: 2019-10-25
+ * Platform:    ATmega328P, 16 MHz, AVR 8-bit Toolchain 3.6.2
+ * ---------------------------------------------------------------------
+ * Description:
+ *    Decimal counter with data output on LCD display.
+ * 
+ * Note:
+ *    Modified version of Peter Fleury's LCD library with R/W pin 
+ *    connected to GND. Newline symbol "\n" is not implemented, use
+ *    lcd_gotoxy() function instead.
+ */
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <stdlib.h>             // itoa() function
+#include "timer.h"
+#include "lcd.h"
+
+/* Typedef -----------------------------------------------------------*/
+/* Define ------------------------------------------------------------*/
+/* Variables ---------------------------------------------------------*/
+/* Function prototypes -----------------------------------------------*/
+
+/* Functions ---------------------------------------------------------*/
+/**
+ *  Brief:  Main program. Shows decimal values ​​on LCD display.
+ *  Input:  None
+ *  Return: None
+ */
+int main(void)
+{
+    /* LCD display
+     * TODO: See Peter Fleury's online manual for LCD library 
+     * http://homepage.hispeed.ch/peterfleury/avr-software.html
+     * Initialize display and test different types of cursor */
+    lcd_init(LCD_DISP_ON);
+
+    // Display string without auto linefeed
+
+    lcd_puts("LCD testing_1");
+    // TODO: Display variable value in decimal, binary, and hexadecimal
+    _delay_ms(4000);
+    lcd_clrscr();
+    lcd_init(LCD_DISP_ON_CURSOR);
+    lcd_gotoxy(3,1);
+    lcd_puts("dO_Ob");
+    lcd_home();
+    lcd_puts("   ___");
+ 
+    _delay_ms(4000);
+    lcd_clrscr();
+    lcd_init(LCD_DISP_ON_CURSOR_BLINK);
+    lcd_puts("LCD testing_3");
+
+    _delay_ms(4000);
+    lcd_clrscr();
+    lcd_init(LCD_DISP_ON_CURSOR);
+    lcd_putc('B');
+    lcd_puts(" LCD testing_4");
+    /* Timer1
+     * TODO: Configure Timer1 clock source and enable overflow 
+     *       interrupt */
+
+    /* TODO: Design at least two user characters and store them in 
+     *       the display memory */
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    for (;;) 
+    {
+
+    }
+
+    // Will never reach this
+    return (0);
+}
+
+/**
+ *  Brief: Timer1 overflow interrupt routine. Increment counter value.
+ */
+ISR(TIMER1_OVF_vect)
+{
+    // TODO: Increment counter value form 0 to 255
+}
