0dd27f6d980ff8ad2ef271d6da94ceec1669335e
Jan Hocz <xhoczj00@vutbr.cz>
2019-11-20 19:46:42 +0100

    GPS Project templates + README
    Created .c file with Makefile + Updated README

---
+++ projects/GPS module project/README.md
@@ -0,0 +1,33 @@
+
+# GPS module project
+
+#### Table of Contents
+
+1. [Introduction](#introduction)
+2. [Hardware](#hardware)
+3. [Software](#software)
+4. [References](#references)
+
+
+## Introduction
+
+Result of this project should be working UART communication between GPS module and microcontroller ATmega328p. 
+Recieved data should be displayed on Nokia 5110 display and stored in PC.
+
+
+## Hardware
+
+Microcontroller - ATmega328p
+GPS Module - ###########
+Display - LCD 5110 (PCD 8544)
+
+## Software
+
+TBD
+
+
+## References
+
+1. Microchip, [AVR 8-bit Toolchain for Windows, Linux, Mac OS X](https://www.microchip.com/mplab/avr-support/avr-and-arm-toolchains-c-compilers)
+2. BANK Larry, [Nokia 5110 C library] (https://github.com/bitbank2/nokia5110)
+3. E-Tinkers, [Nokia5110 LCD Image Creator](https://www.e-tinkers.com/nokia5110-lcd-image-creator/)
---
+++ projects/GPS module project/main.c
@@ -0,0 +1,38 @@
+/*
+ * --------------------------------------------------------------------
+ * Author:      Martin Kosut, Jan Hocz									
+ *             Dept. of Radio Electronics, Brno Univ. of Technology	
+ * Created:     2019-11-20												
+ * Last update: 2019-11-20												
+ * Platform:    ATmega328P, AVR 8-bit Toolchain 3.6.2					
+ * --------------------------------------------------------------------
+ * Description: Communication with GPS module; data -> PC; data -> Nokia 5110 display.
+ * TODO: Make it work.
+ */
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>
+#include <util/delay.h>
+
+/* Typedef -----------------------------------------------------------*/
+/* Define ------------------------------------------------------------*/
+
+/* Variables ---------------------------------------------------------*/
+/* Function prototypes -----------------------------------------------*/
+
+/* Functions ---------------------------------------------------------*/
+
+int main(void)
+{
+    /* Infinite loop */
+    for (;;)
+    {
+       
+	    {
+             
+        }
+       
+    }
+//
+    return (0);
+}
