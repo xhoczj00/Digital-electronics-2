664a56bb074db6edf29f9f12ad562db9170a13f2
lab661 <lab661@vutbr.cz>
2019-11-27 09:51:27 +0100

    [GPS] Reading data from GPS (WIP)

--- projects/GPS module project/main.c
+++ projects/GPS module project/main.c
@@ -15,8 +15,9 @@
 #include <stdbool.h>
 #include <util/delay.h>
 #include "nokia5110.h"
+#include "uart.h"
 
-
+#define UART_BAUD_RATE 9600
 
 
 /* Typedef -----------------------------------------------------------*/
@@ -27,18 +28,42 @@
 
 /* Functions ---------------------------------------------------------*/
 
+
 int main(void)
 {
+    uint8_t S,i;
+    uint8_t COMM[85];
+    
     nokia_lcd_init();
     nokia_lcd_power(1);
-    //nokia_lcd_write_string("aaaaaa", 1);
-    
+    nokia_lcd_write_picture();
+    nokia_lcd_render();
+     _delay_ms(1000);
+     nokia_lcd_clear();
+     nokia_lcd_render();
     /* Infinite loop */
-    char i = 0,j = 0;
+    //char i = 0,j = 0;
+    
+    uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
+
+    uart_puts("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29");
 
     for (;;)
     {
-        for(int k = 0; k<84;k++)
+     S = uart_getc();
+     if (S == '$')
+     {
+        for(i = 0;i<85;i++)
+        {
+            COMM[i] = uart_getc();
+            nokia_lcd_write_char(COMM[i], 1);
+            nokia_lcd_render();
+        }
+        
+        nokia_lcd_clear();
+        nokia_lcd_render();
+     }
+      /*  for(int k = 0; k<84;k++)
         {
             nokia_lcd_set_pixel(i, j, 1);
             _delay_ms(100);
@@ -54,7 +79,7 @@ int main(void)
 	        nokia_lcd_render();
             j++;
         }
-        
+        */
        
         
 
